2abf51c499a7830e38bed82b873be8e2146a06ab
NguyenThiKimLoanB1913243 <LoanB1913243@student.ctu.edu.vn>
2022-09-28 23:21:44 +0700

    Cai dat cac handler

--- app/controllers/contact.controller.js
+++ app/controllers/contact.controller.js
@@ -1,27 +1,43 @@
-exports.create = (req, res) => {
-    res.send({ message: "create handler" });
+const ApiError = require("../api-error");
+const ContactService = require("../services/contact.service");
+const MongoDB = require("../utils/mongodb.util")
+
+//Create and Save a new Contact
+exports.create = async (req, res, next) => {
+    if (!req.body?.name) {
+        return next(new ApiError(400, "Name can not be empty"));
+    }
+
+    try {
+        const contactService = new ContactService(MongoDB.client);
+        const document = await contactService.create(req.body);
+        return res.send(document);
+    } catch (error) {
+        return next(new ApiError(500, "An error occured while creating the contact"));
+    }
 };
 
-exports.findAll = (req, res) => {
-    res.send({ message: "findAll handler" });
+exports.findAll = (req, res) =>{
+    res.send({ mesage: "finfALL handler" });
 };
 
-exports.findOne = (req, res) => {
+exports.findOne = (req, res) =>{
     res.send({ message: "findOne handler" });
 };
 
-exports.update = (req, res) => {
-    res.send({ message: "update handler" });
+exports.update = (req, res) =>{
+    res.send({ mesage: "update handler" });
 };
 
-exports.delete = (req, res) => {
+exports.delete = (req, res) =>{
     res.send({ message: "delete handler" });
 };
 
-exports.deleteAll = (req, res) => {
-    res.send({ message: "deleteAll handler" });
+exports.deleteAll = (req, res) =>{
+    res.send({ mesage: "deleteALL handler" });
+
 };
 
-exports.findAllFavorite = (req, res) => {
-    res.send({ message: "findAllFavorite handler" });
+exports.findAllFavorite = (req, res) =>{
+    res.send({ mesage: "finfALLFavorite handler" });
 };
